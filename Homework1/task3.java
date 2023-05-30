f5c7cba2612066eecc1c7625a2fae7a9ea12db37
Oleh Steshchenko <oleg.steschenko@gmail.com>
2023-05-30 18:07:40 +0300

    first commit

---
+++ Homework1/task3.java
@@ -0,0 +1,17 @@
+package org.example;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+public class task3 {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("\n" + "Write the number of the lesson you want to learn about: ");
+            int a = input.nextInt();
+            a = a * 45 + (a / 2) * 5 + ((a + 1) / 2 - 1) * 15;
+            System.out.println("The lesson ends at: " + (a / 60 + 9 + " " + a % 60));
+        }
+    }
+
+
