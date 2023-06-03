4361c4fbba6085a8571bd975595d76fb61c8f4a7
Oleh Steshchenko <oleg.steschenko@gmail.com>
2023-06-03 11:46:32 +0300

    sdsasf

--- Homework2/src/main/java/org/example/Main.java
+++ Homework2/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helsalvaso world!");
+        System.out.println("Hello dsfsdfworld!");
     }
 }
