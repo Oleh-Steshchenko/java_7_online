4edc6d9b7e8c3195c7cbaeffee5e53de4b97912d
Oleh Steshchenko <oleg.steschenko@gmail.com>
2023-06-03 09:23:16 +0300

    new

--- Homework1/task3.java
+++ Homework1/task3.java
@@ -7,7 +7,7 @@ import java.util.regex.Pattern;
 public class task3 {
         public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
-            System.out.print("\n" + "Write the number of the lesson you want to learn about: ");
+            System.out.print("\n" + "Write  the number of the lesson you want to learn about: ");
             int a = input.nextInt();
             a = a * 45 + (a / 2) * 5 + ((a + 1) / 2 - 1) * 15;
             System.out.println("The lesson ends at: " + (a / 60 + 9 + " " + a % 60));
